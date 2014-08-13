e0143c50f484335059f02d745fb3a69a7b1020fd
J-S-K <kiruj@yandex.ru>
2014-08-13 21:56:53 +0300

    Update main.js

--- TestPlugin/main.js
+++ TestPlugin/main.js
@@ -1,3 +1,20 @@
-Wotg_Plugins.get().addSimplePlugin('TestPlugin', '0.2.2', function (api) {
-	console.log('TestPlugin 0.2.2', { Wotg: Wotg, api: api, atom: atom });
+
+new Wotg.Plugins.Simple({
+	title  : 'TestPlugin',
+	version: '0.2.3'
+}, function (plugin, events) {
+	plugin.addImagesPreload({
+		'test': 'image.png'
+	});
+	
+	console.log('TestPlugin', { Wotg: Wotg, plugin: plugin, atom: atom });
+
+	events.add('initialize', function () {
+		console.log('TestPlugin initialized');
+	});
+
+	events.add('afterLaunch', function () {
+		atom.dom(plugin.getImage('test')).appendTo('body');
+		console.log(plugin.getImage('test'));
+	});
 });
