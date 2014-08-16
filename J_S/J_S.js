ee5944da0d5d09a5f8a25a74be6f3a478f3c5f3d
J-S-K <kiruj@yandex.ru>
2014-08-16 14:09:59 +0300

    Update J_S.js

--- J_S/J_S.js
+++ J_S/J_S.js
@@ -61,8 +61,8 @@ atom.declare( 'Wotg.Research.HqCardItem', Wotg.Research.TreeItem, {
 	}
 });
 	function createCard (data) {
-		var manager = Wotg.Research.Manager();
-		jslog({Wotg.Controller()});
+		//var manager = Wotg.Research.Manager();
+		jslog(Wotg.Controller());
 		jslog({this:this,  manager:manager});
 		var elem = new Wotg.Research.HqCardItem(manager.app.layer, {
 			manager: manager,
