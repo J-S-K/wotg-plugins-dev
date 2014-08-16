6c160c5f3ef555060b8ee78bf27954595da6777c
J-S-K <kiruj@yandex.ru>
2014-08-16 13:11:00 +0300

    Update J_S.js

--- J_S/J_S.js
+++ J_S/J_S.js
@@ -26,6 +26,30 @@ console.log('J_S Plugin', 'test');
 	console.log('J_S afterLaunch');
 	
 	});
+	function createHq (data, isCurrent, isRootTree) {
+		/*
+		var elem = new Wotg.Research.HQItem(this.app.layer, {
+			manager: this,
+			data: data,
+			isCurrent: isCurrent,
+			isRootTree: isRootTree
+		});
+					
+		Wotg.openPopup('ResearchHqSet', {
+				proto: this.proto,
+				manager: this.manager
+			});
+			Wotg.Research.HQSet
+			
+		this.model = new Wotg.Card.Models.HqResearch(this.proto, this.data.exp);
+		this.view = new Wotg.Card.Views.TreeHqOpened(this.model);
+		this.view.events.add('redraw', this.redraw);
+		
+		this.app.mouseHandler.subscribe(elem);
+		this.elems.push(elem);
+		*/
+		
+	}
 	
 	plugin.refactor( 'Wotg.Research.Manager', {
         // Меняем один из методов класса
@@ -36,14 +60,15 @@ console.log('J_S Plugin', 'test');
 		this.selector.hide();
 		this.destroyElems();
 		var list = this.model.getCardListForHQ(hqId);
+		var listHq = this.model.getCardListForHQ(hqId, true);
 		var rootData = this.model.getCardById(hqId);
-		jslog({list:list, rootData:rootData});
-		this.createHq(rootData, true);
+		jslog({list:list, rootData:rootData, listHq:listHq});
+		createHq(rootData, true);
 		for (var i = 0 ; i < list.length; i++) {
 			if (Wotg.controller().protos.get(list[i].card).type.toLowerCase() != 'hq') {
 				this.createCard(list[i]);
 			} else {
-			//	this.createHq(list[i], false, false);
+				this.createHq(list[i], false, false);
 			}
 		}
 		setTimeout(function(){
