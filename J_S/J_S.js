950eb2bcabde849101e2cb59465c32332ddd7670
J-S-K <kiruj@yandex.ru>
2014-08-21 15:20:20 +0300

    Update J_S.js

--- J_S/J_S.js
+++ J_S/J_S.js
@@ -200,7 +200,7 @@ atom.declare( 'Wotg.Research.HqCardItem', Wotg.Research.CardItem, {
 		jslog(this.model, this.defaultNation );
 		
 		var list = this.model.getTreeByNation(this.defaultNation );
-		list.sort (function(a,b {
+		list.sort (function(a,b) {
 			return a.proto.level > b.proto.level
 		}))
 		for (var i = 0 ; i < list.length; i++) { //list.length
